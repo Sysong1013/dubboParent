1d47ef99475c918f802361c4eb52626b22319bd5
unknown <songyisong@dangdang.com>
2018-03-29 19:14:34 +0800

    WMS接口dubbo测试

--- dubbo-client/src/main/java/com/demo/springdubbo/DubboClient.java
+++ dubbo-client/src/main/java/com/demo/springdubbo/DubboClient.java
@@ -1,17 +1,24 @@
 package com.demo.springdubbo;
 
 import com.alibaba.dubbo.container.spring.SpringContainer;
+import com.dangdang.wms.interlayer.stock.facade.RemotePinBackFacade;
+import com.dangdang.wms.interlayer.stock.model.RemotePinBackReturnBean;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 public class DubboClient {
 
     public static void main(String[] args) {
         SpringContainer providerContainer = new SpringContainer();
         providerContainer.start();
 
-        BlockchainService blockchainService = SpringContainer.getContext().getBean(BlockchainService.class);
+        RemotePinBackFacade RemotePinBackFacade = SpringContainer.getContext().getBean(RemotePinBackFacade.class);
         try {
-            blockchainService.chain();
+            RemotePinBackReturnBean remotePinBackReturnBean= RemotePinBackFacade.getOrderFromErp("13034376492");
+            System.out.println(remotePinBackReturnBean.getResponseInfo());
+            System.out.println(remotePinBackReturnBean.getOrderMainDetail().getChildList().get(0).getItemId());
+            System.out.println(remotePinBackReturnBean.getStockRecordList());
+            System.out.println(remotePinBackReturnBean.getInvoiceMainDetail());
         }catch (Exception e){
             System.out.println(e);
         }
